23adb9cbff023cb4a4b9c4369f67d21aa80769ba
sushant690 <sushantchoudhary321@gmail.com>
2025-04-12 05:44:14 +0000

    Day1_Afternoon

---
+++ dotnetapp/Services/IAuthService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+
+namespace dotnetapp.Services
+{
+    public interface IAuthService
+    {
+        Task<(int,string)> Registration(User )
+    } 
+}
