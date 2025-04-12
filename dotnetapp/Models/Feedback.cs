318ccd967f711791b3eedd1eb834aa838fa3a48d
Sourabh-Singh-372 <singhsourabh372@gmail.com>
2025-04-12 06:15:51 +0000

    FeedbackCode

--- dotnetapp/Models/Feedback.cs
+++ dotnetapp/Models/Feedback.cs
@@ -7,9 +7,10 @@ namespace dotnetapp.Models
 {
     public class Feedback
     {
-        public int Feedback {get;set;}
+        public int FeedbackId {get;set;}
         public int UserId {get;set;}
         public User? User {get;set;}
         public string FeedbackText {get;set;}
+        public DateTime Date {get;set;}
     }
 }
