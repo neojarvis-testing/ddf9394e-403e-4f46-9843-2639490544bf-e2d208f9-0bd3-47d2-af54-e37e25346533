89490bf60323827887da459f84dd555c8242e09e
Sourabh-Singh-372 <singhsourabh372@gmail.com>
2025-04-14 10:23:26 +0000

    BackendAuth

--- dotnetapp/Models/User.cs
+++ dotnetapp/Models/User.cs
@@ -2,17 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace dotnetapp.Models
 {
     public class User
     {
-        public int UserId {get; set;}
-        public string Email {get; set;}
-        public string Password {get; set;}
-        public string Username {get; set;}
-        public string MobileNumber {get; set;}
-        public string UserRole {get; set;}
+        public int UserId { get; set; }
+ 
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please provide a valid email address.")]
+        public string Email { get; set; }
+ 
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+ 
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+ 
+        [Required(ErrorMessage = "Mobile number is required.")]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Please provide a valid 10-digit mobile number.")]
+        public string MobileNumber { get; set; }
+ 
+        [Required(ErrorMessage = "User role is required.")]
+        public string UserRole { get; set; }
         public string? SecretKey {get; set;}
     }
 }
