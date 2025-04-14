3f796699236d034a4ed965f90786c30c9f38ee63
anshulpathrabe <anshulhpathrabe@gmail.com>
2025-04-14 10:23:33 +0000

    registration component DONE!!

--- angularapp/src/app/components/registration/registration.component.ts
+++ angularapp/src/app/components/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  user: User = {
+    UserId: 0,
+    Email: '',
+    Password: '',
+    Username: '',
+    MobileNumber: '',
+    UserRole: '',
+    SecretKey: ''
+  };
+
+  confirmPassword:'';
+  passwordMisMatch: boolean = false;
+  secretKeyMisMatch: boolean = false;
+  secretKeyRequired: boolean = false;
+  adminSecretKey = 'Zion'; //Hardcode
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  checkRole()
+  {
+    if(this.user.UserRole == 'Admin')
+    {
+      this.secretKeyRequired = true;
+    }
+  }
+
+  register()
+  {
+    alert('added successfully');
+    if(this.user.Password != this.confirmPassword && this.user.SecretKey != this.adminSecretKey)
+    {
+      this.passwordMisMatch = true;
+      this.secretKeyMisMatch = true;
+      return;
+    }
+
+    this.authService.register(this.user).subscribe( data =>{
+      this.user = data;
+      this.router.navigate(['/login']);
+      
+    });
+  }
 }
+
+
+
+
--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -11,7 +11,7 @@ import { Login } from '../models/login.model';
 })
 export class AuthService {
 
-  public apiUrl = 'https://ide-aeeaedafcfecdbdfcfafebbbfeedfbddafee.premiumproject.examly.io/proxy/8080/';
+  public apiUrl = 'https://ide-cdedfabbeefdaaecdbdfcfafebbbfeedfbddafee.premiumproject.examly.io/proxy/8080/';
 
   private userRoleSubject = new BehaviorSubject<string | null>(null);
  
