a370c3ab7ddd588b1f83b430f710b62ecbe0044d
anshulpathrabe <anshulhpathrabe@gmail.com>
2025-04-15 09:22:20 +0000

    admin edit mentorship component

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { ViewmentorshipprogramComponent } from './components/viewmentorshipprogram/viewmentorshipprogram.component';
+import { MentorshipapplicationlistComponent } from './components/mentorshipapplicationlist/mentorshipapplicationlist.component';
+import { AdmineditmentorshipprogramComponent } from './components/admineditmentorshipprogram/admineditmentorshipprogram.component';
 
 const routes: Routes = [
   // {path:'', component:RegistrationComponent},
-  {path:'', component:ViewmentorshipprogramComponent}
+  // {path:'', component:ViewmentorshipprogramComponent}
+  // {path:'', component:MentorshipapplicationlistComponent}
+  {path:'', component:AdmineditmentorshipprogramComponent}
 
 ];
 
