89dc92f649a5361d67c619217b8a92daad3804b8
Sruthikha <sruthikhar@gmail.com>
2025-04-15 07:11:03 +0000

    done user except feedback

--- angularapp/src/app/components/userappliedmentorshipapplication/userappliedmentorshipapplication.component.ts
+++ angularapp/src/app/components/userappliedmentorshipapplication/userappliedmentorshipapplication.component.ts
@@ -9,7 +9,65 @@ import { Router } from '@angular/router';
 })
 export class UserappliedmentorshipapplicationComponent implements OnInit {
   ngOnInit(): void {
+  }
+  mentorships = [
 
+    {
+  
+     programName: 'Data Career Mentorship',
+  
+     profileImage: 'assets/profile1.png',
+  
+     applicationDate: '2025-04-10',
+  
+     status: 'Pending'
+  
+    }
+  
+   ];
+  
+  
+  
+   searchTerm = '';
+  
+   popupImage: string | null = null;
+  
+  
+  
+   get filteredMentorships() {
+  
+    return this.mentorships.filter(m =>
+  
+     m.programName.toLowerCase().includes(this.searchTerm.toLowerCase())
+  
+    );
+  
+   }
+  
+  
+  
+   viewImage(imageUrl: string) {
+  
+    this.popupImage = imageUrl;
+  
+   }
+  
+  
+  
+   confirmDelete(index: number) {
+  
+    const confirmDelete = confirm('Are you sure you want to delete this application?');
+  
+    if (confirmDelete) {
+  
+     this.mentorships.splice(index, 1);
+  
+    }
+  
+   }
+  
   }
-    
-  }
+  
+  
+  
+  
