e051f22561442b77d9bc9532fc781c5b03047028
Deepanjali95 <deepanjali95z@gmail.com>
2025-04-15 06:35:41 +0000

    first commit

--- angularapp/src/app/components/creatementorshipprogram/creatementorshipprogram.component.spec.ts
+++ angularapp/src/app/components/creatementorshipprogram/creatementorshipprogram.component.spec.ts
@@ -1,25 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, throwError } from 'rxjs';
 import { CreatementorshipprogramComponent } from './creatementorshipprogram.component';
+import { MentorshipService } from 'src/app/services/mentorship.service';
+import { Router } from '@angular/router';
 
-describe('CreatementorshipprogramComponent', () => {
+describe('CreateMentorshipprogramComponent', () => {
   let component: CreatementorshipprogramComponent;
   let fixture: ComponentFixture<CreatementorshipprogramComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CreatementorshipprogramComponent ]
-    })
-    .compileComponents();
-  });
+  let mentorshipService: jasmine.SpyObj<MentorshipService>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
+    const mentorshipServiceSpy = jasmine.createSpyObj('MentorshipService', ['addMentorshipprogram']);
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      declarations: [CreatementorshipprogramComponent],
+      imports: [FormsModule, RouterTestingModule],
+      providers: [
+        { provide: MentorshipService, useValue: mentorshipServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
+    });
+
     fixture = TestBed.createComponent(CreatementorshipprogramComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    mentorshipService = TestBed.inject(MentorshipService) as jasmine.SpyObj<MentorshipService>;
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
   });
 
-  it('should create', () => {
+  fit('Frontend_should_create_creatementorshipprogram_component', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('Frontend_should_contain_create_new_mentorship_program_heading_in_the_creatementorshipprogram_component', () => {
+    const componentHTML = fixture.debugElement.nativeElement.outerHTML;
+    expect(componentHTML).toContain('Create New Mentorship Program');
+  });
+
 });
