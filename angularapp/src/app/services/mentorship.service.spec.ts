375a380569d57d00901f033cae4287a27c82172f
Sourabh-Singh-372 <singhsourabh372@gmail.com>
2025-04-15 05:07:31 +0000

    FinalSecurityBackendCode

--- angularapp/src/app/services/mentorship.service.spec.ts
+++ angularapp/src/app/services/mentorship.service.spec.ts
@@ -1,16 +1,20 @@
+
 import { TestBed } from '@angular/core/testing';
 
 import { MentorshipService } from './mentorship.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('MentorshipService', () => {
   let service: MentorshipService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(MentorshipService);
   });
 
-  it('should be created', () => {
+  fit('Frontend_should_create_mentorship_service', () => {
     expect(service).toBeTruthy();
   });
 });
